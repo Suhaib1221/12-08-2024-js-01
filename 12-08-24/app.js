c36ea90594678f387520ff814e53c030802eb810
Muhammad Suhaib <120370619+Suhaib1221@users.noreply.github.com>
2024-08-17 19:07:34 +0500

    files added

---
+++ 12-08-24/app.js
@@ -0,0 +1,60 @@
+// // ==================// Assignment # 1====================
+// // =================// JAVASCRIPT===========================
+// // ================// ALERTS!===========================
+
+alert("Welcome to Js land")
+alert("prevent this page from creating additional dialogs.")
+alert("Welcome to Js land\n Happy Coding!")
+console.log("this is in console")
+
+// // ==================// Assignment # 1====================
+// // =================// JAVASCRIPT===========================
+// // ================// End===========================
+
+// // ==================// Assignment # 2====================
+// // =================// JAVASCRIPT===========================
+// // ================// VARIABLES FOR STRINGS!===========================
+
+var fullName = "Muhammad Suhaib";
+var age = "19 year old";
+var passion = "web developer";
+var lineBreake = "Pizza\nPizz\nPiz\nPi";
+var email = "suhaib1514@gmail.com";
+var book = "A smarter way to learn JavaScript";
+var html = "I cane write html throug javascript.<br><br>";
+var symbol = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+
+alert(fullName);
+alert(age);
+alert(passion);
+alert(lineBreake);
+alert(email);
+alert(book)
+alert(symbol)
+document.write(html)
+
+// // ==================// Assignment # 2====================
+// // =================// JAVASCRIPT===========================
+// // ================// VARIABLES FOR STRINGS!===========================
+// // ===============// End===========================
+
+// // ==================// Assignment # 3====================
+// // =================// JAVASCRIPT===========================
+// // ================// VARIABLES FOR NUMBERS!===========================
+// // ===============// start===========================
+
+var birthYear = 2005;
+var visitorName = "<b>Muhammad Suhaib</b>";
+var productTitle = "<b>T-shirt(s)</b>";
+var quantity = "<b>5</b>";
+
+document.write(" My birth year is: " + birthYear + "<br><br>");
+
+document.write(visitorName + " ordered " + quantity + productTitle +" "+ " on xyz clothing store.<br>");
+
+
+// // ==================// Assignment # 3====================
+// // =================// JAVASCRIPT===========================
+// // ================// VARIABLES FOR NUMBERS!===========================
+// // ===============// End===========================
