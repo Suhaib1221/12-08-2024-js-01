6dbf896b317cca6f7a4a02311b97beb99e948b6f
Muhammad Suhaib <120370619+Suhaib1221@users.noreply.github.com>
2024-09-13 16:28:54 +0500

    files added

---
+++ 10-09-2024/app.js
@@ -0,0 +1,87 @@
+// // ==================// Assignment #10====================
+// // =================// JAVASCRIPT===========================
+// // ================// USER INPUT & CONDITIONAL STATEMENT ===========================
+// // ================// Start ===========================
+
+// Question:01
+
+// var city = prompt("Enter city name")
+
+// if(city == "karachi"){
+//     alert("Welcome to city of light")
+// }
+
+// Question:02
+
+// var gender = prompt("Enter your gender")
+
+// if(gender == "male"){
+//     alert("Good Morning Sir")
+// }else if (gender == "female"){
+//     alert("Good Morning Miss")
+// }else{
+//     alert("Enter your gender for output")
+// }
+
+// Question:03
+
+// var userInput = prompt("Enter traffic signal color");
+
+// if(userInput == "red"){
+//     alert("Must stop")
+// }
+// else if(userInput == "yellow"){
+//     alert("Ready to move")
+// }
+// else if(userInput == "green"){
+//     alert("Move now")
+// }else{
+//     alert("Enter traffic signal color for output")
+// }
+
+// Question:04
+
+// var remainingFuel = +prompt("Enter remaning fuel","In liters")
+
+// if(remainingFuel <= 1){
+//     alert("Your fuel is low please refill you tank to the nearest gas station")
+// }
+// else if(remainingFuel > 1){
+//     alert("Your fuel is good no need for refill")
+// }
+// else{
+//     alert("Please input your remaning fuel")
+// }
+
+// Question:05
+
+
+// var a = 4;
+// if (++a === 5) {
+//     alert("given condition for variable a is true");
+// }
+
+
+// Question:06
+
+// var guessNumber = +prompt("Enter your guess number")
+
+// if (guessNumber === 7) {
+//     alert("Bingo this is correct")
+// } else if (guessNumber > 7) {
+//     alert("Your guess number is high")
+// }
+// else {
+//     alert("Enter your guess number for output")
+// }
+
+// Question:07
+
+var input = prompt("Enter your number to find even or odd")
+var evenNumber = 2
+var oddNumber = 1
+
+for(var i = 0; i ===0 ; i++){
+    +evenNumber 
+}
+
